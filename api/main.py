1b31ab7adc96dd950bf5e1303a3731348d6f8ba6
m.malyshkina <mar_mal05@mail.ru>
2023-04-22 17:06:22 +0300

    handles initialization of application and the connection to the database

---
+++ api/main.py
@@ -0,0 +1,15 @@
+import sys
+sys.path.append('/home/riverhals/Documents/Kasper/infohub/')
+from fastapi import FastAPI
+from api.routers import documentation, process_control, process_status, process_result
+
+app = FastAPI()
+
+@app.get("/")
+async def read_root():
+    return "Server is running"
+
+app.include_router(documentation.router)
+app.include_router(process_control.router)
+app.include_router(process_status.router)
+app.include_router(process_result.router)
