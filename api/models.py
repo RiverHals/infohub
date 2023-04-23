59154eb3e6fe5004a33d17da7a37419ff2d6ae44
m.malyshkina <mar_mal05@mail.ru>
2023-04-23 14:25:19 +0300

    specified process attributes

--- api/models.py
+++ api/models.py
@@ -1,4 +1,5 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class Process(BaseModel):
-     name: str 
+     name: str = Field(..., title="The name of the process", min_length=1, max_length=150)
+     # body: str = Field(..., title="The description of the process", min_length=1, max_length=100)
