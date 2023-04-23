14ba38e271ea61453d7a2fc62dbb9b6cd80648ae
m.malyshkina <mar_mal05@mail.ru>
2023-04-23 14:25:54 +0300

    start_process, stop_process

--- api/services/process_manager.py
+++ api/services/process_manager.py
@@ -1,21 +1,45 @@
-from typing import Optional
+# from typing import Optional
+import subprocess
+import asyncio
 import sys
 sys.path.append('/home/riverhals/Documents/Kasper/infohub/')
 from api.models import Process
 
 class ProcessManager:
     processes = {}
-
-    def start_process(self, process: Process):
+    run = []
+    async def start_process(self, process: Process):
         if process.name not in self.processes:
-            self.processes[process.name] = True
+            try:
+                # await asyncio.create_subprocess_shell(process.name, stdout=asyncio.subprocess.PIPE,
+                #                                                 stderr=asyncio.subprocess.PIPE)
+                subprocess.run(process.name, shell=True, check=True, capture_output=True, text=True)
+                self.processes.append([process.name])
+                return True
+
+            except subprocess.CalledProcessError:
+                print("Command failed to execute.")
+            except Exception as e:
+                print("An error occurred: ", e)
             return True
         else:
             return False
 
-    def stop_process(self, process: Process):
+    async def stop_process(self, process: Process):
         if process.name in self.processes:
-            self.processes[process.name] = False
+            try:
+                pid_list = subprocess.check_output(["pidof", process.name])
+                pids = [int(pid) for pid in pid_list.decode().split()]
+                for pid in pids:
+                    subprocess.check_call(["kill", str(pid)])
+
+                print(f"Command '{process.name}' terminated successfully.")
+                self.processes[process.name] = False
+
+            except subprocess.CalledProcessError as e:
+                print(f"Error terminating command '{process.name}': {e}")
+            except Exception as e:
+                print("An error occurred: ", e)
             return True
         else:
             return False
